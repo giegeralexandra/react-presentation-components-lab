3f030c31886a6c120db6e692325fd3dee965aa89
giegeralexandra <giegeralexandra@gmail.com>
2021-05-02 12:52:16 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
 // Code SimplerComponent Here
+import { render } from 'enzyme';
+import React from 'react';
+import SimpleComponent from './SimpleComponent'
+
+const SimplerComponent = (props) => 
+        <div onClick={props.handleClick} >I am just happy</div>
+;
+
+
+export default SimplerComponent
